a3132baa802475dfae5c319f657a78d62f79aae7
Helloworldzz <58582177+ProgramOfRobot@users.noreply.github.com>
2021-05-02 11:24:19 +0800

    Test.

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("A+B")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+})
+radio.setGroup(1)
